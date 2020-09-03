7e7fd9b5f6106e851cb330e0487c3a34d43f1875
shannmc <shannonmcnellis2@gmail.com>
2020-09-03 19:42:42 -0400

    initial commit

---
+++ src/main/java/services/KindergartenPassingCalculatorSC.java
@@ -0,0 +1,7 @@
+package services;
+
+public class KindergartenPassingCalculatorSC implements PassingCalculator{
+    public boolean passOrFail(Score score) {
+        return score.getMathScore() > 70 && score.getReadingScore() > 70;
+    }
+}
---
+++ src/main/java/services/PassingCalculator.java
@@ -0,0 +1,5 @@
+package services;
+
+public interface PassingCalculator {
+    boolean passOrFail(Score score);
+}
